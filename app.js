d19fc99af4f5b9221222b3e745a096389a86cd13
Ogunleye Emmanuel <Anuolu1645@gmail.com>
2023-11-23 21:44:36 +0100

    feat: Created class for request controller

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const dbPath = path.join(__dirname, "DB", "users.json");
 
 const PORT = 3000;
 
-const books = [
-  { id: 1, name: "The good book", author: "Arebi" },
-  { id: 2, name: "The bad book", author: "chemi" },
-];
+const users = [,];
 
 function requestHandler(req, res) {
+  const controller = new requestController(req, res);
   res.setHeader("Content-Type", "application/json");
-  if (req.method === "GET" && req.url === "/books") requestController(req, res);
+
+  if (req.method === "GET" && req.url === "/users") {
+    controller.getAllUsers();
+  } else if (req.method === "POST" && req.url === "/users") {
+  }
 }
 
-const requestController = async (req, res) => {
-  if (!books) {
-    res.writeHead(404);
-    res.end(JSON.stringify("Book Not found"));
+class requestController {
+  constructor(req, res) {
+    this.req = req;
+    this.res = res;
   }
-  const response = await books;
-  res.writeHead(200);
-  res.end(JSON.stringify(books));
-};
+
+  getAllUsers() {
+    if (!users) {
+      res.writeHead(404);
+      res.end(JSON.stringify("User Not found"));
+    }
+
+    fs.readFile(dbPath, "utf-8", (err, data) => {
+      if (err) {
+        console.log("An Error Ocurred");
+        this.res.writeHead(500);
+        this.res.end(JSON.stringify("Error occured while reading db"));
+      }
+
+      this.res.writeHead(200);
+      this.res.end(data);
+    });
+  }
+
+  createUser() {}
+}
 
 const server = http.createServer(requestHandler);
 
