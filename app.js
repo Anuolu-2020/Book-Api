9cd14564bca80c06e881367a6101951bc1d587b9
Ogunleye Emmanuel <Anuolu1645@gmail.com>
2023-11-24 17:55:17 +0100

    feat: Implement users api and book api

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require("http");
-const fs = require("fs");
 const path = require("path");
-
-const dbPath = path.join(__dirname, "DB", "users.json");
+const fs = require("fs");
 
 const PORT = 3000;
 
-const users = [,];
+const usersDB = path.join(__dirname, "DB", "users.json");
+
+const bookDB = path.join(__dirname, "DB", "books.json");
 
 function requestHandler(req, res) {
   const controller = new requestController(req, res);
@@ -16,8 +16,14 @@ function requestHandler(req, res) {
     controller.getAllUsers();
   } else if (req.url === "/users" && req.method === "POST") {
     controller.createUser();
+  } else if (req.url === "/books" && req.method === "POST") {
+    controller.createBook();
   }
 }
+
+// function authenticateUser(req, res) {
+//   return new Promise((resolve, reject) => {});
+// }
 
 class requestController {
   constructor(req, res) {
@@ -26,19 +32,14 @@ class requestController {
   }
 
   getAllUsers() {
-    if (!users) {
-      this.res.writeHead(404);
-      this.res.end(JSON.stringify("User Not found"));
-    }
-
-    fs.readFile(dbPath, "utf-8", (err, data) => {
+    fs.readFile(usersDB, "utf-8", (err, data) => {
       if (err) {
         console.log("An Error Ocurred");
         this.res.writeHead(500);
         this.res.end(JSON.stringify("Error occured while reading db"));
       }
 
-      this.res.writeHead(200);
+      this.res.statusCode = 200;
       this.res.end(data);
     });
   }
@@ -59,26 +60,30 @@ class requestController {
         return;
       }
 
-      const newBook = JSON.parse(parsedBody);
+      const newUser = JSON.parse(parsedBody);
 
-      fs.readFile(dbPath, "utf-8", (err, data) => {
+      fs.readFile(usersDB, "utf-8", (err, data) => {
         if (err) {
           console.log(err);
           this.res.writeHead(400);
           this.res.end("An error occured");
         }
 
-        const oldBooks = JSON.parse(data);
+        const users = JSON.parse(data);
 
-        const bookLength = oldBooks.length - 1;
+        const lastUserIndex = users.length - 1;
 
-        const lastItem = oldBooks[bookLength];
+        const lastUser = users[lastUserIndex];
 
-        newBook.id = lastItem.id + 1;
+        if (lastUserIndex == -1) {
+          newUser.id = 1;
+        } else {
+          newUser.id = lastUser.id + 1;
+        }
 
-        const allbooks = [...oldBooks, newBook];
+        const allUsers = [...users, newUser];
 
-        fs.writeFile(dbPath, JSON.stringify(allbooks), (err) => {
+        fs.writeFile(usersDB, JSON.stringify(allUsers), (err) => {
           if (err) {
             console.log(err);
             this.res.writeHead(400);
@@ -86,10 +91,52 @@ class requestController {
           }
 
           this.res.writeHead(201);
-          this.res.end({
-            message: "User Created successfully",
-            data: JSON.stringify(allbooks),
-          });
+          this.res.end(JSON.stringify(allUsers));
+        });
+      });
+    });
+  }
+
+  createBook() {
+    const body = [];
+
+    this.req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    this.req.on("end", () => {
+      const reqBody = Buffer.concat(body).toString();
+
+      const newBook = JSON.parse(reqBody);
+
+      if (!reqBody) {
+        this.res.writeHead(400);
+        this.res.end("No data received");
+        return;
+      }
+
+      fs.readFile(bookDB, "utf-8", (err, data) => {
+        const oldBooks = JSON.parse(data);
+
+        const lastBookIndex = oldBooks.length - 1;
+
+        const lastItem = oldBooks[lastBookIndex];
+
+        if (lastBookIndex == -1) {
+          newBook.id = 1;
+        } else {
+          newBook.id = lastItem.id + 1;
+        }
+
+        fs.writeFile(bookDB, JSON.stringify(newBook), (err) => {
+          if (err) {
+            console.log(err);
+            this.res.statusCode = 500;
+            this.res.end("An error occured");
+          }
+
+          this.res.statusCode = 201;
+          this.res.end(JSON.stringify(newBook));
         });
       });
     });
